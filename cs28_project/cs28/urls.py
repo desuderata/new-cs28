c6f3eb692619de18075863e1de7e0f7dc2ded967
Yee Hou Teoh <2471020t@student.gla.ac.uk>
2021-01-20 14:27:35 +0800

    Added Logout
    Users can now log out
    
    - Added a logout button to the navbar
    - Added link to admin page in the navbar
    - Added login button if user is not logged in

--- cs28_project/cs28/urls.py
+++ cs28_project/cs28/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('cs28', include('django.contrib.auth.urls')),
     path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
