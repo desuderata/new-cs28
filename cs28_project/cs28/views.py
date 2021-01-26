d8d1b348c2762c5de9bd8f59be9f7b503195c6db
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2021-01-26 23:27:28 +0100

    Created new view for module_marks_upload
    Added a new view for module_marks_upload in views.py.

--- cs28_project/cs28/views.py
+++ cs28_project/cs28/views.py
@@ -4,11 +4,14 @@ todo:
 - change index to render instead of HttpResponse
 
 author: Yee Hou, Teoh (2471020t)
+        Nguyen Thanh Hieu (2401707n)
         # add yr name here if you are working on this file.
 """
+import csv, io
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from django.urls import reverse
 from django.http import HttpResponse
 
@@ -44,3 +47,31 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('cs28:index'))
+    
+@permission_required('admin.can_add_log_entry')
+def module_marks_upload(request):
+    template = "module_marks_upload.html"
+    
+    prompt = {
+        'order': 'Order of the CSV should be coureCode, matricNo, alphanum'
+    }
+    
+    if request.method == "GET":
+        return render(request, template, prompt)
+    
+    csv_file = request.FILES['file']
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+    
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter = ',', quotechar = '|'):
+        _, created = Grade.objects.update_or_create(
+            courseCode=column[0],
+            matricNo=column[1],
+            alphanum=column[2]
+        )
+        
+        context = {}
+        return render(request, template, context)
