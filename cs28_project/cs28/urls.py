6305c4b69431f3e027b42f9bf24d450fb045ca67
KienWelch <59999504+KienWelch@users.noreply.github.com>
2021-01-26 17:36:30 +0000

    Created student upload page

--- cs28_project/cs28/urls.py
+++ cs28_project/cs28/urls.py
@@ -2,6 +2,7 @@
 
 author: Yee Hou, Teoh (2471020t)
         # add yr name here if you are working on this file.
+        Kien Welch 2371692w
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -14,4 +15,6 @@ urlpatterns = [
     # path('cs28', include('django.contrib.auth.urls')),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('studentUpload/', views.studentUpload, name='studentUpload'),
+    
 ]
