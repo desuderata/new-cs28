ce337dee2e12de021f0dcd04d1c92652a000ba30
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2021-02-01 17:30:29 +0100

    Update views.py
    Made a module marks upload view for the functionality of  uploading module marks using csv files.

--- cs28_project/cs28/views.py
+++ cs28_project/cs28/views.py
@@ -60,3 +60,31 @@ def manage(request):
 @login_required
 def module_grades(request):
     return render(request, 'module_grades.html')
+
+@permission_required('admin.can_add_log_entry')
+def module_marks_upload(request):
+    template = "module_marks_upload.html"
+    
+    prompt = {
+        'order': 'Order of the CSV should be courseCode, matricNo, alphanum'
+    }
+    
+    if request.method == "GET":
+        return render(request, template, prompt)
+    
+    csv_file = request.FILES['file']
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+    
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    for column in csv.reader(io_string, delimiter = ',', quotechar = '|'):
+        _, created = Grade.objects.update_or_create(
+            courseCode=column[0],
+            matricNo=column[1],
+            alphanum=column[2]
+        )
+        
+        context = {}
+        return render(request, template, context)
