d65c1622cd916d3fdf63c32f0f9db14f9f66b039
Yee Hou Teoh <2471020t@student.gla.ac.uk>
2021-02-19 22:08:38 +0800

    Changes to table in manage.html
    Changes to the functions of the table in manage.html
    
    - Rows are colored red if there are missing grades.
    - Exportable as pdf now
    - Exports the entire table now
    - Added a dictionary to convert grades to alphanum
    - Hide unnecessary fields

--- cs28_project/cs28/admin.py
+++ cs28_project/cs28/admin.py
@@ -1,5 +1,3 @@
-# from django.contrib import admin
-# from cs28.models import Grade, Student, AcademicPlan, GraduationYear
 # Register your models here.
 
 from django.contrib.admin import ModelAdmin, register
